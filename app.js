569e728cf0e5066563ac5d2f691c6ac0bad0ae19
rodrigosousa80 <rodrigo.sousa.80@gmail.com>
2022-12-24 14:33:05 -0300

    adicionando um console log

--- app.js
+++ app.js
@@ -1,1 +1,5 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
+console.log("Aplicação está se conectando ao banco de dados")
+// mais um console log de teste:
+console.log("O Paulo fez esse commit sem você ver na aula!")
+console.log("Enviando para o GitHub")
