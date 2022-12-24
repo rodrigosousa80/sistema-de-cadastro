a92813acdddbd506b5af31ec66133efa519c9116
rodrigosousa80 <rodrigo.sousa.80@gmail.com>
2022-12-24 15:02:33 -0300

    voltando para o estado do paulo

--- app.js
+++ app.js
@@ -1,5 +1,1 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
-console.log("Aplicação está se conectando ao banco de dados")
-// mais um console log de teste:
-console.log("O Paulo fez esse commit sem você ver na aula!")
-console.log("Enviando para o GitHub")
